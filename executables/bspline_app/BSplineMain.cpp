3d244cf9fe584aa179e96639f1cb0f3dd766018c
Mohammad Fakhreddin <mohammad.fakhreddin@gmail.com>
2023-10-24 14:26:05 -0600

    Engine files are added

---
+++ executables/bspline_app/BSplineMain.cpp
@@ -0,0 +1,16 @@
+#include "BedrockLog.hpp"
+
+#include "BedrockMath.hpp"
+#include "BufferTracker.hpp"
+// #include "BSplineApp.hpp"
+
+int main()
+{
+    // {
+    //     BSplineApp app{};
+    //     app.Run();
+    // }
+    printf("Hi\n");
+    
+    return 0;
+}
