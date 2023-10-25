1aa4feaf3133b36fa8e6c35a0191de787b7ac360
Mohammad Fakhreddin <mohammad.fakhreddin@gmail.com>
2023-10-25 17:14:46 -0600

    Some work on bsplines but I'm not sure if it is need

--- shared/BSplineCurve.hpp
+++ shared/BSplineCurve.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
-class BSplineCurve
+#include <glm/glm.hpp>
+#include <vector>
+// TODO: Implementing a BSpline curve is a different assignment
+namespace Shared::BSplineCurve
 {
 
-};
+    using ControlPoints = std::vector<glm::vec3>;
+    using KnotSequence = std::vector<float>;
+    using Curve = std::vector<glm::vec3>;
+
+    void GenerateCurve(
+        ControlPoints const & cps, 
+        KnotSequence const & knotSeq,
+        float const uStep, 
+        int const order,
+        Curve & outCurve
+    )
+    {
+
+    }
+
+    void GenerateUniformKnotSequence()
+    {
+         
+
+    }
+
+}
--- shared/ControlPoint.hpp
+++ shared/ControlPoint.hpp
@@ -1,6 +1,27 @@
 #pragma once
 
-class ControlPoint
+#include <glm/glm.hpp>
+
+namespace Shared
 {
+
+    class ControlPoint
+    {
+    public:
+    
+        explicit ControlPoint(glm::vec3 position);
+
+        [[nodiscard]]
+        glm::vec3 const & GetPosition();
+
+        void SetPosition(glm::vec3 position);
+
+    private:
+
+        int _idx{};
+        glm::vec3 _position{};
+        float _weight = 1.0f;
+        
+    };
 
 };
